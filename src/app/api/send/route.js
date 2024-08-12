ff4b7019f3c1a5bc516ad6951a5359b8e559115d
Nikhil Baisoya <nikhilbaisoya9@gmail.com>
2024-08-12 11:48:40 +0530

    feat: image changes

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('https://api.emailjs.com/api/v1.0/email/send');
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
